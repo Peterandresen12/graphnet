ed8b4d20881344bfff5f6b817fbb5d61e6bc49ef
Peterandresen12 <pqb128@alumni.ku.dk>
2022-11-28 09:56:05 +0100

    changed detector

--- src/graphnet/models/detector/icecube.py
+++ src/graphnet/models/detector/icecube.py
@@ -77,6 +77,39 @@ class IceCubeDeepCore(IceCube86):
         return data
 
 
+class IceCubeDeepCore_std_scaler(IceCube86):
+    """`Detector` class for IceCube-DeepCore."""
+
+    # Implementing abstract class attribute
+    features = FEATURES.DEEPCORE
+
+    def _forward(self, data: Data) -> Data:
+        """Ingest data, build graph, and preprocess features.
+
+        Args:
+            data: Input graph data.
+
+        Returns:
+            Connected and preprocessed graph data.
+        """
+        # Check(s)
+        self._validate_features(data)
+
+        # Preprocessing
+        data.x[:, 0] /= 100.0  # dom_x
+        data.x[:, 1] /= 100.0  # dom_y
+        data.x[:, 2] += 350.0  # dom_z
+        data.x[:, 2] /= 100.0
+        data.x[:, 3] /= 1.05e04  # dom_time
+        data.x[:, 3] -= 1.0
+        data.x[:, 3] *= 20.0
+        data.x[:, 4] /= 1.0  # charge
+        data.x[:, 5] -= 1.25  # rde
+        data.x[:, 5] /= 0.25
+        data.x[:, 6] /= 0.05  # pmt_area
+
+        return data
+
 class IceCubeUpgrade(IceCubeDeepCore):
     """`Detector` class for IceCube-Upgrade."""
 
